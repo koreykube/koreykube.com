c74689cd6e8ce75cba902b7bc819c9fa81f192fe
Korey Kube <koreykube@gmail.com>
2020-05-29 19:52:33 +1000

    Main website body complete

---
+++ src/views/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <h3>Get in touch</h3>
+    <div class="info-box">
+      <p class="title">Korey Kube</p>
+      <p class="additional">52a Vale Street</p>
+      <p class="additional">St Kilda, VIC, 3182</p>
+      <p class="additional"><span class="highlight">E: </span>koreykube@gmail.com</p>
+      <p class="additional"><span class="highlight">P: </span>0401 956 300</p>
+    </div>
+    <div class="form-box">
+      <form action="mailto:koreykube@gmail.com" method="post" enctype="text/plain">
+        <input type="text" name="name" required placeholder="Name"><br/>
+        <input type="email" name="email" required placeholder="you@yourdomin.com"><br/>
+        <textarea type="textarea" rows="5" name="message" placeholder="Type your message here..."></textarea>
+        <br/>
+        <div class="action-buttons">
+          <input type="submit" value="Send">
+          <input type="reset" value="Clear">
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <h3>Freelance software and web developer based in Melbourne.<br/> Highly experienced in Vue.JS and C#.</h3>
+    <div class="text-body">
+      <p>Hi there! I'm Korey.</p> 
+      <p>I fell in to coding making a small match-3 game for my Bejewelled-obsessed partner to announce our wedding.</p> 
+      <p>Since then I've been growing as a developer with a focus on front-end development for Vue based web applications as well as AWS S3 and Serverless for back end.</p> 
+      <p>I still make small games for friends, family and loved ones on special occasions and even you if you ask nicely.</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
